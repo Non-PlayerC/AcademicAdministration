67589d4c6d2cd3b8332dceed6f18dc06427f3603
npc <1700969761@qq.com>
2023-03-10 10:29:00 +0800

    controller 测试前

--- education-back/education-back/src/test/java/com/ye/back/EducationBackApplicationTests.java
+++ education-back/education-back/src/test/java/com/ye/back/EducationBackApplicationTests.java
@@ -1,13 +1,29 @@
 package com.ye.back;
 
+import com.ye.back.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.prefs.BackingStoreException;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = EducationBackApplication.class)
 class EducationBackApplicationTests {
+    @Autowired
+    private UserMapper userMapper;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testMapper() {
+        System.out.println(userMapper.selectById(1));
     }
 
 }
